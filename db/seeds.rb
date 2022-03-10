0be5a918c0858beec6766f8346c4d236aef19bfa
enzostk <enzostk@gmail.com>
2022-03-10 23:32:05 +0100

    zeub

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.destroy_all
-Task.destroy_all
-
 require 'faker'
-3.times do
-  my_category = Category.create(title: Faker::Book.genre)
-  3.times do
-    my_task = Task.new(title: Faker::Book.title,
-                      deadline: Faker::Date.forward(days:23),
-                      image: Faker::Avatar.image)
-    my_task.category = my_category
-    my_task.save
+Email.destroy_all
+5.times do
+    email = Email.create(
+      object: Faker::Artist.name,
+      body: Faker::ChuckNorris.fact,
+      )
   end
-end
